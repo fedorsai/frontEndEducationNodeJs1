7d30ac33c850de9aa09575d3269a3229f306872c
Фёдор <sfedor2004@gmail.com>
2017-05-25 12:57:23 +0400

    авторизация в ВК

--- server.js
+++ server.js
@@ -2,42 +2,40 @@
  * Created by Фёдор on 21.05.2017.
  */
 const express = require("express");
+const vk = require('./vkMuthods.js');
 
 const app = express();
+const vkUrl = 'https://oauth.vk.com/authorize?client_id=6046102&redirect_uri=http://localhost:3000/vk&display=popup&scope=groupsresponse_type=code&v=5.64';
 app.set('view engine', 'jade');
+
 const port = 3000;
 const axios = require("axios");
 function run() {
-   //* "02H05KUjCQlduyqfFcS2JMWRR",
-   //     "N4oBDCI6M40MaFVOQyyCjCimMH7UygFUUjZA23jT9JmXhqNTYl",
-
 
     app.get('/', function (request, response) {
         response.render('index', { title: 'Fucking Twitter'});
-        //response.send(getAuthForm());
-
         response.end();
-
     });
 
-    app.post('/', function (request, response) {
-
-        response.send("Авторизация в твиттере");
-//axios.get('https://api.twitter.com/oauth/authenticate?oauth_token=866261745656090626-WgWVyytFOaSW0YCd4elebnSTv3I9mRR')
+    app.get('/login', function (request, response) {
+        response.redirect(vkUrl);
         response.end();
+    });
 
+    app.get('/vk', function (request, response) {
+        console.log(request.query.code);
+        vk.getGroupInfo(request, response);
+    });
+
+
+    app.post('/', function (request, response) {
+        response.redirect('/login');
+        response.end();
     });
 
     app.use(function (req, res) {
         res.send("Страница не доступна!")
     });
-/*
-    var getAuthForm = function () {
-        return '<form action="/" method="post">'+
-                '<input type="submit" text="Авторизация"/></form>'
-    }
-*/
-
 
     app.listen(port, function () {
         console.log("Server Ok.")
